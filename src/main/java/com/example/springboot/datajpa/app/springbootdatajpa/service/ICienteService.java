e6beb1fe5103053b6d62709f71fde9442342ed8c
Vexturion <horizontedesucesos@outlook.com>
2023-07-06 00:39:14 -0600

    Paginador
    se agrega clase y layout de paginador para paginar los resultados de las consultas

--- src/main/java/com/example/springboot/datajpa/app/springbootdatajpa/service/ICienteService.java
+++ src/main/java/com/example/springboot/datajpa/app/springbootdatajpa/service/ICienteService.java
@@ -2,11 +2,16 @@ package com.example.springboot.datajpa.app.springbootdatajpa.service;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 import com.example.springboot.datajpa.app.springbootdatajpa.models.entity.Cliente;
 
 public interface ICienteService {
 
     public List<Cliente> findAll();
+
+    public Page<Cliente> findAll(Pageable pageable);
 
     public void save(Cliente cliente);
 
---
+++ src/main/java/com/example/springboot/datajpa/app/springbootdatajpa/util/paginator/PageItem.java
@@ -0,0 +1,18 @@
+package com.example.springboot.datajpa.app.springbootdatajpa.util.paginator;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class PageItem {
+
+    private int numero;
+
+    private boolean actual;
+
+
+
+}
